886820d003081e3dde63ddc6b66e7ccaa4e4816d
Austin Fahsl <austin@fahsl.io>
2023-02-10 17:36:39 -0500

    chore: add eslint (#1)
    * chore: add eslint
    
    * chore: rename CI jobs

---
+++ packages/random-emoji/.eslintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: ['./tsconfig.json'],
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['@typescript-eslint'],
+  root: true,
+};
