8cdcadc2ab7be7192c8dc1b0593aef37ff66c685
Austin Fahsl <austin@fahsl.io>
2023-02-17 11:02:00 -0700

    chore: fix formatting

--- packages/emoji-math/__tests__/evaluate.spec.ts
+++ packages/emoji-math/__tests__/evaluate.spec.ts
@@ -2,8 +2,8 @@ import { evaluate } from '../lib/evaluate';
 
 describe('evaluate', () => {
   it('should evaluate an expression', () => {
-    const result = evaluate("1️⃣2️⃣➕2️⃣✖️4️⃣");
+    const result = evaluate('1️⃣2️⃣➕2️⃣✖️4️⃣');
 
-    expect(result).toEqual("2️⃣0️⃣");
+    expect(result).toEqual('2️⃣0️⃣');
   });
 });
--- packages/emoji-math/lib/evaluate.ts
+++ packages/emoji-math/lib/evaluate.ts
@@ -1,14 +1,17 @@
 import { symbols } from './symbols';
 
-const toString = (symbol: string): string | undefined => symbols.find(pair => pair[0] === symbol)?.[1];
-const toSymbol = (str: string): string | undefined => symbols.find(pair => pair[1] === str)?.[0];
+const toString = (symbol: string): string | undefined =>
+  symbols.find((pair) => pair[0] === symbol)?.[1];
+const toSymbol = (str: string): string | undefined =>
+  symbols.find((pair) => pair[1] === str)?.[0];
 
 export function evaluate(expression: string): string {
   let expressionString = expression;
-  for (const symbol in symbols) {
-    expressionString = expressionString.replace(symbol[0], symbol[1]);
-  }
+  symbols.forEach(
+    (symbol) =>
+      (expressionString = expressionString.replace(symbol[0], symbol[1]))
+  );
   return expressionString;
 }
 
-console.log(evaluate("1️⃣2️⃣➕2️⃣✖️4️⃣"))
+console.log(evaluate('1️⃣2️⃣➕2️⃣✖️4️⃣'));
